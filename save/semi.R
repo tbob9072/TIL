22da7300a5e6114d75be9c7b9df04320f2cdd85b
tbob <tbob9072@gmail.com>
2019-12-23 16:31:46 +0900

    Add semi.md

---
+++ save/semi.R
@@ -0,0 +1,41 @@
+setwd("c://Rwork")
+train <- read.csv("train.csv")
+head(train)
+test <- read.csv("hoho.csv")
+head(test)
+str(train)
+
+library(dplyr)
+
+train$성별 <- as.factor(train$성별)
+train$혼인상태 <- as.factor(train$혼인상태)
+train$주택형태 <- as.factor(train$주택형태)
+train$종교 <- as.factor(train$종교)
+train$봉사 <- as.factor(train$봉사)
+train$기부 <- as.factor(train$기부)
+train$정치성향 <- as.factor(train$정치성향)
+train$직업 <- as.factor(train$직업)
+
+test$성별 <- as.factor(test$성별)
+test$혼인상태 <- as.factor(test$혼인상태)
+test$주택형태 <- as.factor(test$주택형태)
+test$종교 <- as.factor(test$종교)
+test$봉사 <- as.factor(test$봉사)
+test$기부 <- as.factor(test$기부)
+test$정치성향 <- as.factor(test$정치성향)
+test$직업 <- as.factor(test$직업)
+
+
+train <- train %>% select(-c(id, 자치구, 가족신뢰도, 공공기관신뢰도, 종합신뢰도, 신뢰도))
+test <- test %>% select(-c(id, 자치구, 가족신뢰도, 공공기관신뢰도, 종합신뢰도, 신뢰도))
+
+library(nnet)
+model <- multinom(이웃신뢰도~., data = train)
+# summary(model)
+result <- predict(model, test)
+result
+
+confusionMatrix(result, as.factor(test$이웃신뢰도))
+
+
+
