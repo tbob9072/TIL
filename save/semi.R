dced391adc540bc4218f2d073a5668bf510531bb
김재현 <tbob@ip-70-12-228-188.kscymo.spcsdns.net>
2019-12-27 17:20:32 +0900

    Modify semi.R

--- save/semi.R
+++ save/semi.R
@@ -26,7 +26,7 @@ test$정치성향 <- as.factor(test$정치성향)
 test$직업 <- as.factor(test$직업)
 
 
-train <- train %>% select(-c(id, 자치구, 가족신뢰도, 공공기관신뢰도, 종합신뢰도, 신뢰도))
+train <- train %>% select(-c(id, 자치구, 가족신뢰도, 공공기관신뢰도, 종합신뢰도))
 test <- test %>% select(-c(id, 자치구, 가족신뢰도, 공공기관신뢰도, 종합신뢰도, 신뢰도))
 
 library(nnet)
@@ -37,5 +37,19 @@ result
 
 confusionMatrix(result, as.factor(test$이웃신뢰도))
 
+train_fe <- train %>% filter(성별 == 2)
+mean(train_fe$이웃신뢰도)
+train_fe
 
+train_ma <- train %>% filter(성별 == 1)  
+train_ma
+
+hist(train_fe$이웃신뢰도)
+hist(train_ma$이웃신뢰도)
+t.test(train_fe$이웃신뢰도, train_ma$이웃신뢰도)
+var.test(train_fe$이웃신뢰도, train_ma$이웃신뢰도)
+
+t.test(train$성별, train$이웃신뢰도, paired = T)
+head(train)
+str(train)
 
