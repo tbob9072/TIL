4d4eec7a084d5645fdf549e49a6340ddad4ce2df
김재현 <tbob@ip-70-12-228-188.kscymo.spcsdns.net>
2020-01-02 21:44:28 +0900

    Modify semi.R

--- save/semi.R
+++ save/semi.R
@@ -7,6 +7,9 @@ str(train)
 library(caret)
 library(dplyr)
 View(train)
+colSums(is.na(train))
+str(train)
+
 
 train$성별 <- as.factor(train$성별)
 train$혼인상태 <- as.factor(train$혼인상태)
@@ -45,11 +48,17 @@ confusionMatrix(result, test$가족신뢰도)
 
 # 의사결정트리 머신러닝
 library(rpart)
+install.packages("party")
+library(party)
 
-model <- rpart(가족신뢰도~., data = train)
+model <- rpart(이웃신뢰도~., data = train)
+model <- ctree(이웃신뢰도~., data = train)
 result <- predict(model, test, type = "class")
 head(result)
-confusionMatrix(result, test$가족신뢰도)
+confusionMatrix(result, test$이웃신뢰도)
+plot(model)
+
+
 
 #랜덤포레스트
 install.packages("randomForest")
@@ -105,3 +114,69 @@ length(nnet.pred)
 length(test$이웃신뢰도)
 
 confusionMatrix(factor(nnet.pred, levels = c(1, 2, 3, 4, 5)), test$이웃신뢰도)
+
+
+#PCA 분석
+result<-prcomp(na.omit(train),scale = T)
+result
+summary(result)
+biplot(result)
+screeplot(result,npcs=4,type="lines",main="Score")
+
+#교차분석
+install.packages("gmodels")
+library(gmodels)
+CrossTable(train$성별, train$이웃신뢰도, expected = T, chisq = T) # low
+CrossTable(train$연령, train$이웃신뢰도, expected = T) # low
+CrossTable(train$학력, train$이웃신뢰도, expected = T) # low
+CrossTable(train$소득, train$이웃신뢰도, expected = T) # low
+CrossTable(train$혼인상태, train$이웃신뢰도, expected = T) # low
+CrossTable(train$주택형태, train$이웃신뢰도, expected = T) # low
+CrossTable(train$종교, train$이웃신뢰도, expected = T) # low
+CrossTable(train$서울고향, train$이웃신뢰도, expected = T) # low
+CrossTable(train$행복_건강, train$이웃신뢰도, expected = T) # low
+CrossTable(train$행복_재정, train$이웃신뢰도, expected = T) # low
+CrossTable(train$행복_친구, train$이웃신뢰도, expected = T) # low
+CrossTable(train$행복_가정, train$이웃신뢰도, expected = T) # low
+CrossTable(train$행복_사회, train$이웃신뢰도, expected = T) # low
+CrossTable(train$봉사, train$이웃신뢰도, expected = T) # low
+CrossTable(train$기부, train$이웃신뢰도, expected = T) # low
+CrossTable(train$사회적지위, train$이웃신뢰도, expected = T) # low
+CrossTable(train$약자_장애, train$이웃신뢰도, expected = T) # low
+CrossTable(train$약자_제도, train$이웃신뢰도, expected = T) # low
+CrossTable(train$약자_노인, train$이웃신뢰도, expected = T) # low
+CrossTable(train$약자_여성, train$이웃신뢰도, expected = T) # low
+CrossTable(train$외국인_친구, train$이웃신뢰도, expected = T) # low
+CrossTable(train$외국인_이웃, train$이웃신뢰도, expected = T) # low
+CrossTable(train$외국인_가족, train$이웃신뢰도, expected = T) # low
+CrossTable(train$정치성향, train$이웃신뢰도, expected = T) # low
+CrossTable(train$직업, train$이웃신뢰도, expected = T) # low
+
+
+
+
+train
+train <- train %>% select(-c(id, 자치구, 가족신뢰도, 공공기관신뢰도, 이웃신뢰도))
+str(train)
+
+t<- lm(종합신뢰도~., data=train)
+summary(t)
+
+step(t, direction = 'both')
+
+par(mfrow=c(2,2))
+plot(t)
+
+t_model <- lm(formula = 종합신뢰도 ~ 성별 + 연령 + 학력 + 소득 + 
+                혼인상태 + 주택형태 + 종교 + 서울고향 + 행복_건강 + 
+                행복_재정 + 행복_친구 + 기부 + 사회적지위 + 
+                약자_장애 + 약자_제도 + 약자_노인 + 약자_여성 + 
+                외국인_친구 + 외국인_이웃 + 외국인_가족 + 
+                정치성향 + 직업, data = train)
+
+summary(t_model)
+plot(t_model)
+par(mfrow=c(1,1))
+
+
+
